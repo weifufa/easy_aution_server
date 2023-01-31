bf2543c32deacf12a4c30a4710cc83851662b093
weifufa <1461531243@qq.com>
2023-01-31 22:39:18 +0800

    添加后台类别管理

--- easyaution-product/src/main/java/com/weifufa/easyaution/product/EasyautionProductApplication.java
+++ easyaution-product/src/main/java/com/weifufa/easyaution/product/EasyautionProductApplication.java
@@ -2,7 +2,11 @@ package com.weifufa.easyaution.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EasyautionProductApplication {
 
