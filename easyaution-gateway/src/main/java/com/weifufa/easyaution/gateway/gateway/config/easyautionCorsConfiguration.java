0a8aff4a598f4d0d6fd3c6aafd1f4a0eeecd6fec
weifufa <1461531243@qq.com>
2023-01-31 19:29:39 +0800

    解决双重跨域问题

--- easyaution-gateway/src/main/java/com/weifufa/easyaution/gateway/gateway/config/easyautionCorsConfiguration.java
+++ easyaution-gateway/src/main/java/com/weifufa/easyaution/gateway/gateway/config/easyautionCorsConfiguration.java
@@ -8,16 +8,16 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class easyautionCorsConfiguration {
-//    @Bean
-//    public CorsWebFilter corsWebFilter() {
-//        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        //1.配置跨域
-//        corsConfiguration.addAllowedHeader("*");//允许跨域头
-//        corsConfiguration.addAllowedMethod("*");//允许哪些请求方式
-//        corsConfiguration.addAllowedOrigin("*");//允许请求来源
-//        corsConfiguration.setAllowCredentials(true);//是否允许携带cookie跨域
-//        source.registerCorsConfiguration("/**",corsConfiguration);
-//        return new CorsWebFilter(source);
-//    }
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //1.配置跨域
+        corsConfiguration.addAllowedHeader("*");//允许跨域头
+        corsConfiguration.addAllowedMethod("*");//允许哪些请求方式
+        corsConfiguration.addAllowedOrigin("*");//允许请求来源
+        corsConfiguration.setAllowCredentials(true);//是否允许携带cookie跨域
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
 }
