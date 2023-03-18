288ec12f8e2722c1d26f9d1cfd759fd348a2a1c7
韦ff <1461531243@qq.com>
2023-03-18 23:05:44 +0800

    竞拍管理界面

--- src/views/modules/adm/product/auction-add-or-update.vue
+++ src/views/modules/adm/product/auction-add-or-update.vue
@@ -2,35 +2,63 @@
   <el-dialog
     :title="!dataForm.auctionId ? '新增' : '修改'"
     :close-on-click-modal="false"
-    :visible.sync="visible">
-    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
-    <el-form-item label="拍卖品名称" prop="auctionName">
-      <el-input v-model="dataForm.auctionName" placeholder="拍卖品名称"></el-input>
-    </el-form-item>
-    <el-form-item label="拍卖数量" prop="auctionAmount">
-      <el-input v-model="dataForm.auctionAmount" placeholder="拍卖数量"></el-input>
-    </el-form-item>
-    <el-form-item label="起拍价" prop="startPrice">
-      <el-input v-model="dataForm.startPrice" placeholder="起拍价"></el-input>
-    </el-form-item>
-    <el-form-item label="竞拍次数" prop="auctionNumber">
-      <el-input v-model="dataForm.auctionNumber" placeholder="竞拍次数"></el-input>
-    </el-form-item>
-    <el-form-item label="最高价" prop="maxPrice">
-      <el-input v-model="dataForm.maxPrice" placeholder="最高价"></el-input>
-    </el-form-item>
-    <el-form-item label="竞拍状态" prop="auctionState">
-      <el-input v-model="dataForm.auctionState" placeholder="竞拍状态"></el-input>
-    </el-form-item>
-    <el-form-item label="结束时间" prop="endTime">
-      <el-input v-model="dataForm.endTime" placeholder="结束时间"></el-input>
-    </el-form-item>
-    <el-form-item label="发布时间" prop="createTime">
-      <el-input v-model="dataForm.createTime" placeholder="发布时间"></el-input>
-    </el-form-item>
-    <el-form-item label="修改时间" prop="updateTime">
-      <el-input v-model="dataForm.updateTime" placeholder="修改时间"></el-input>
-    </el-form-item>
+    :visible.sync="visible"
+  >
+    <el-form
+      :model="dataForm"
+      :rules="dataRule"
+      ref="dataForm"
+      @keyup.enter.native="dataFormSubmit()"
+      label-width="80px"
+    >
+      <el-form-item label="拍品名称" prop="auctionName" style="width: 400px">
+        <el-input
+          v-model="dataForm.auctionName"
+          placeholder="拍卖品名称"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="文字描述" prop="remark" style="width: 400px">
+        <el-input type="textarea" v-model="dataForm.remark"></el-input>
+      </el-form-item>
+      <el-form-item label="拍品图片">
+        <multi-upload v-model="dataForm.images"></multi-upload>
+      </el-form-item>
+      <el-form-item label="拍卖数量" prop="auctionAmount">
+        <el-input-number
+          v-model="dataForm.auctionAmount"
+          :min="1"
+          :max="10"
+          :step="1"
+        ></el-input-number>
+      </el-form-item>
+      <el-form-item label="起拍价" prop="startPrice" style="width: 280px">
+        <el-input
+          v-model="dataForm.startPrice"
+          type="number"
+          :min="0"
+          :max="9999.99"
+          :step="100"
+          placeholder="请输入价格"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="开拍时间" prop="auctionStartTime">
+        <el-date-picker
+          v-model="dataForm.auctionStartTime"
+          type="datetime"
+          placeholder="选择日期时间"
+          @change="startTimeChange"
+        >
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item label="结束时间" prop="auctionEndTime">
+        <el-date-picker
+          v-model="dataForm.auctionEndTime"
+          type="datetime"
+          placeholder="选择日期时间"
+          @change="endTimeChange"
+        >
+        </el-date-picker>
+      </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
       <el-button @click="visible = false">取消</el-button>
@@ -40,117 +68,136 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        visible: false,
-        dataForm: {
-          auctionId: 0,
-          auctionName: '',
-          auctionAmount: '',
-          startPrice: '',
-          auctionNumber: '',
-          maxPrice: '',
-          auctionState: '',
-          endTime: '',
-          createTime: '',
-          updateTime: ''
-        },
-        dataRule: {
-          auctionName: [
-            { required: true, message: '拍卖品名称不能为空', trigger: 'blur' }
-          ],
-          auctionAmount: [
-            { required: true, message: '拍卖数量不能为空', trigger: 'blur' }
-          ],
-          startPrice: [
-            { required: true, message: '起拍价不能为空', trigger: 'blur' }
-          ],
-          auctionNumber: [
-            { required: true, message: '竞拍次数不能为空', trigger: 'blur' }
-          ],
-          maxPrice: [
-            { required: true, message: '最高价不能为空', trigger: 'blur' }
-          ],
-          auctionState: [
-            { required: true, message: '竞拍状态不能为空', trigger: 'blur' }
-          ],
-          endTime: [
-            { required: true, message: '结束时间不能为空', trigger: 'blur' }
-          ],
-          createTime: [
-            { required: true, message: '发布时间不能为空', trigger: 'blur' }
-          ],
-          updateTime: [
-            { required: true, message: '修改时间不能为空', trigger: 'blur' }
-          ]
+import MultiUpload from "@/components/upload/multiUpload";
+export default {
+  components: { MultiUpload },
+  data() {
+    return {
+      visible: false,
+      dataForm: {
+        remark: "",
+        images: [],
+        auctionId: 0,
+        auctionName: "",
+        auctionAmount: "",
+        startPrice: "",
+        auctionNumber: "",
+        maxPrice: "",
+        auctionState: "",
+        auctionEndTime: "",
+        auctionStartTime: "",
+      },
+      dataRule: {
+        auctionName: [
+          { required: true, message: "拍卖品名称不能为空", trigger: "blur" },
+        ],
+        auctionAmount: [
+          { required: true, message: "拍卖数量不能为空", trigger: "blur" },
+        ],
+        startPrice: [
+          { required: true, message: "起拍价不能为空", trigger: "blur" },
+        ],
+        auctionEndTime: [
+          { required: true, message: "请选择结束时间", trigger: "blur" },
+        ],
+        auctionStartTime: [
+          { required: true, message: "请选择开始时间", trigger: "blur" },
+        ],
+        remark: [
+          { required: true, message: "请填写拍品说明", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  methods: {
+    init(id) {
+      this.dataForm.auctionId = id || 0;
+      this.visible = true;
+      this.$nextTick(() => {
+        // this.$refs["dataForm"].resetFields();
+        if (this.dataForm.auctionId) {
+          this.$http({
+            url: this.$http.adornUrl(
+              `/product/auction/info/${this.dataForm.auctionId}`
+            ),
+            method: "get",
+            params: this.$http.adornParams(),
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.dataForm.auctionName = data.auction.auctionName;
+              this.dataForm.auctionAmount = data.auction.auctionAmount;
+              this.dataForm.startPrice = data.auction.startPrice;
+              this.dataForm.auctionNumber = data.auction.auctionNumber;
+              this.dataForm.maxPrice = data.auction.maxPrice;
+              this.dataForm.auctionState = data.auction.auctionState;
+              this.dataForm.endTime = data.auction.endTime;
+              this.dataForm.createTime = data.auction.createTime;
+              this.dataForm.updateTime = data.auction.updateTime;
+            }
+          });
         }
+      });
+    },
+    // 表单提交
+    dataFormSubmit() {
+      this.$refs["dataForm"].validate((valid) => {
+        if (valid) {
+          this.$http({
+            url: this.$http.adornUrl(
+              `/product/auction/${!this.dataForm.auctionId ? "save" : "update"}`
+            ),
+            method: "post",
+            data: this.$http.adornData({
+              auctionId: this.dataForm.auctionId || undefined,
+              auctionName: this.dataForm.auctionName,
+              auctionAmount: this.dataForm.auctionAmount,
+              startPrice: this.dataForm.startPrice,
+              auctionNumber: this.dataForm.auctionNumber,
+              maxPrice: this.dataForm.maxPrice,
+              auctionState: this.dataForm.auctionState,
+              auctionEndTime: this.dataForm.auctionEndTime,
+              auctionStartTime: this.dataForm.auctionStartTime,
+              remark: this.dataForm.remark,
+              images: JSON.stringify(this.dataForm.images),
+            }),
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: "操作成功",
+                type: "success",
+                duration: 1500,
+                onClose: () => {
+                  this.visible = false;
+                  this.$emit("refreshDataList");
+                },
+              });
+            } else {
+              this.$message.error(data.msg);
+            }
+          });
+        }
+      });
+    },
+    startTimeChange() {
+      if (
+        this.dataForm.auctionEndTime &&
+        this.dataForm.auctionStartTime >= this.dataForm.auctionEndTime
+      ) {
+        debugger;
+        this.$message.error("开始时间不能大于等结束时间");
+        this.dataForm.auctionStartTime = "";
       }
     },
-    methods: {
-      init (id) {
-        this.dataForm.auctionId = id || 0
-        this.visible = true
-        this.$nextTick(() => {
-          this.$refs['dataForm'].resetFields()
-          if (this.dataForm.auctionId) {
-            this.$http({
-              url: this.$http.adornUrl(`/product/auction/info/${this.dataForm.auctionId}`),
-              method: 'get',
-              params: this.$http.adornParams()
-            }).then(({data}) => {
-              if (data && data.code === 0) {
-                this.dataForm.auctionName = data.auction.auctionName
-                this.dataForm.auctionAmount = data.auction.auctionAmount
-                this.dataForm.startPrice = data.auction.startPrice
-                this.dataForm.auctionNumber = data.auction.auctionNumber
-                this.dataForm.maxPrice = data.auction.maxPrice
-                this.dataForm.auctionState = data.auction.auctionState
-                this.dataForm.endTime = data.auction.endTime
-                this.dataForm.createTime = data.auction.createTime
-                this.dataForm.updateTime = data.auction.updateTime
-              }
-            })
-          }
-        })
-      },
-      // 表单提交
-      dataFormSubmit () {
-        this.$refs['dataForm'].validate((valid) => {
-          if (valid) {
-            this.$http({
-              url: this.$http.adornUrl(`/product/auction/${!this.dataForm.auctionId ? 'save' : 'update'}`),
-              method: 'post',
-              data: this.$http.adornData({
-                'auctionId': this.dataForm.auctionId || undefined,
-                'auctionName': this.dataForm.auctionName,
-                'auctionAmount': this.dataForm.auctionAmount,
-                'startPrice': this.dataForm.startPrice,
-                'auctionNumber': this.dataForm.auctionNumber,
-                'maxPrice': this.dataForm.maxPrice,
-                'auctionState': this.dataForm.auctionState,
-                'endTime': this.dataForm.endTime,
-                'createTime': this.dataForm.createTime,
-                'updateTime': this.dataForm.updateTime
-              })
-            }).then(({data}) => {
-              if (data && data.code === 0) {
-                this.$message({
-                  message: '操作成功',
-                  type: 'success',
-                  duration: 1500,
-                  onClose: () => {
-                    this.visible = false
-                    this.$emit('refreshDataList')
-                  }
-                })
-              } else {
-                this.$message.error(data.msg)
-              }
-            })
-          }
-        })
+    endTimeChange() {
+      if (
+        this.dataForm.auctionStartTime &&
+        this.dataForm.auctionEndTime <= this.dataForm.auctionStartTime
+      ) {
+        debugger;
+        this.$message.error("结束时间不能小于等于开始时间");
+        this.dataForm.auctionEndTime = "";
       }
-    }
-  }
+    },
+  },
+};
 </script>
--- src/views/modules/adm/product/auction.vue
+++ src/views/modules/adm/product/auction.vue
@@ -1,62 +1,177 @@
 <template>
   <div class="mod-config">
-    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+    <el-form
+      :inline="true"
+      :model="dataForm"
+      @keyup.enter.native="getDataList()"
+    >
       <el-form-item>
-        <el-input v-model="dataForm.key" placeholder="参数名" clearable></el-input>
+        <el-input
+          v-model="dataForm.key"
+          placeholder="参数名"
+          clearable
+        ></el-input>
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
         <el-button type="primary" @click="addOrUpdateHandle()">新增</el-button>
-        <el-button @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
+        <el-button
+          type="danger"
+          @click="deleteHandle()"
+          :disabled="dataListSelections.length <= 0"
+          >批量删除</el-button
+        >
       </el-form-item>
     </el-form>
-    <el-table :data="dataList" border v-loading="dataListLoading" @selection-change="selectionChangeHandle"
-      style="width: 100%;">
-      <el-table-column type="selection" header-align="center" align="center" width="50">
+    <el-table
+      :data="dataList"
+      border
+      v-loading="dataListLoading"
+      @selection-change="selectionChangeHandle"
+      style="width: 100%"
+    >
+      <el-table-column
+        type="selection"
+        header-align="center"
+        align="center"
+        width="50"
+      >
       </el-table-column>
-      <el-table-column prop="auctionId" header-align="center" align="center" label="拍品id">
+      <el-table-column
+        prop="auctionId"
+        header-align="center"
+        align="center"
+        label="拍品id"
+      >
       </el-table-column>
-      <el-table-column prop="auctionName" header-align="center" align="center" label="拍卖品名称">
+      <el-table-column
+        prop="auctionName"
+        header-align="center"
+        align="center"
+        label="拍卖品名称"
+      >
       </el-table-column>
-      <el-table-column prop="auctionAmount" header-align="center" align="center" label="拍卖数量">
+      <el-table-column
+        prop="auctionAmount"
+        header-align="center"
+        align="center"
+        label="拍卖数量"
+      >
       </el-table-column>
-      <el-table-column prop="startPrice" header-align="center" align="center" label="起拍价">
+      <el-table-column
+        prop="startPrice"
+        header-align="center"
+        align="center"
+        label="起拍价"
+      >
       </el-table-column>
-      <el-table-column prop="auctionNumber" header-align="center" align="center" label="竞拍次数">
+      <el-table-column
+        prop="auctionNumber"
+        header-align="center"
+        align="center"
+        label="竞拍次数"
+      >
       </el-table-column>
-      <el-table-column prop="maxPrice" header-align="center" align="center" label="最高价">
+      <el-table-column
+        prop="maxPrice"
+        header-align="center"
+        align="center"
+        label="最高价"
+      >
       </el-table-column>
-      <el-table-column prop="auctionState" header-align="center" align="center" label="竞拍状态">
-      </el-table-column>
-      <el-table-column prop="endTime" header-align="center" align="center" label="结束时间">
-      </el-table-column>
-      <el-table-column prop="createTime" header-align="center" align="center" label="发布时间">
-      </el-table-column>
-      <el-table-column prop="updateTime" header-align="center" align="center" label="修改时间">
-      </el-table-column>
-      <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
+      <el-table-column
+        prop="auctionState"
+        header-align="center"
+        align="center"
+        label="竞拍状态"
+      >
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.auctionId)">修改</el-button>
-          <el-button type="text" size="small" @click="deleteHandle(scope.row.auctionId)">删除</el-button>
+          <el-tag
+            :type="
+              scope.row.auctionState == '0'
+                ? ''
+                : scope.row.auctionState == '1'
+                ? 'success'
+                : 'info'
+            "
+            :effect="dark"
+          >
+            {{
+              scope.row.auctionState == 0
+                ? "未开拍"
+                : scope.row.auctionState == 1
+                ? "竞拍中"
+                : "已结束"
+            }}</el-tag
+          >
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="auctionStartTime"
+        header-align="center"
+        align="center"
+        value-format="yyyy-MM-dd HH:mm:ss"
+        label="开始时间"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="auctionEndTime"
+        header-align="center"
+        align="center"
+        value-format="yyyy-MM-dd HH:mm:ss"
+        label="结束时间"
+      >
+      </el-table-column>
+
+      <el-table-column
+        fixed="right"
+        header-align="center"
+        align="center"
+        width="150"
+        label="操作"
+      >
+        <template slot-scope="scope">
+          <el-button
+            type="text"
+            size="small"
+            @click="addOrUpdateHandle(scope.row.auctionId)"
+            >修改</el-button
+          >
+          <el-button
+            type="text"
+            size="small"
+            @click="deleteHandle(scope.row.auctionId)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination @size-change="sizeChangeHandle" @current-change="currentChangeHandle" :current-page="pageIndex"
-      :page-sizes="[10, 20, 50, 100]" :page-size="pageSize" :total="totalPage"
-      layout="total, sizes, prev, pager, next, jumper">
+    <el-pagination
+      @size-change="sizeChangeHandle"
+      @current-change="currentChangeHandle"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      :total="totalPage"
+      layout="total, sizes, prev, pager, next, jumper"
+    >
     </el-pagination>
     <!-- 弹窗, 新增 / 修改 -->
-    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+    <add-or-update
+      v-if="addOrUpdateVisible"
+      ref="addOrUpdate"
+      @refreshDataList="getDataList"
+    ></add-or-update>
   </div>
 </template>
 
 <script>
-import AddOrUpdate from './auction-add-or-update'
+import AddOrUpdate from "./auction-add-or-update";
 export default {
   data() {
     return {
       dataForm: {
-        key: ''
+        key: "",
       },
       dataList: [],
       pageIndex: 1,
@@ -64,90 +179,96 @@ export default {
       totalPage: 0,
       dataListLoading: false,
       dataListSelections: [],
-      addOrUpdateVisible: false
-    }
+      addOrUpdateVisible: false,
+    };
   },
   components: {
-    AddOrUpdate
+    AddOrUpdate,
   },
   activated() {
-    this.getDataList()
+    this.getDataList();
   },
   methods: {
     // 获取数据列表
     getDataList() {
-      this.dataListLoading = true
+      this.dataListLoading = true;
       this.$http({
-        url: this.$http.adornUrl('/product/auction/list'),
-        method: 'get',
+        url: this.$http.adornUrl("/product/auction/list"),
+        method: "get",
         params: this.$http.adornParams({
-          'page': this.pageIndex,
-          'limit': this.pageSize,
-          'key': this.dataForm.key
-        })
+          page: this.pageIndex,
+          limit: this.pageSize,
+          key: this.dataForm.key,
+        }),
       }).then(({ data }) => {
         if (data && data.code === 0) {
-          this.dataList = data.page.list
-          this.totalPage = data.page.totalCount
+          this.dataList = data.page.list;
+          this.totalPage = data.page.totalCount;
         } else {
-          this.dataList = []
-          this.totalPage = 0
+          this.dataList = [];
+          this.totalPage = 0;
         }
-        this.dataListLoading = false
-      })
+        this.dataListLoading = false;
+      });
     },
     // 每页数
     sizeChangeHandle(val) {
-      this.pageSize = val
-      this.pageIndex = 1
-      this.getDataList()
+      this.pageSize = val;
+      this.pageIndex = 1;
+      this.getDataList();
     },
     // 当前页
     currentChangeHandle(val) {
-      this.pageIndex = val
-      this.getDataList()
+      this.pageIndex = val;
+      this.getDataList();
     },
     // 多选
     selectionChangeHandle(val) {
-      this.dataListSelections = val
+      this.dataListSelections = val;
     },
     // 新增 / 修改
     addOrUpdateHandle(id) {
-      this.addOrUpdateVisible = true
+      this.addOrUpdateVisible = true;
       this.$nextTick(() => {
-        this.$refs.addOrUpdate.init(id)
-      })
+        this.$refs.addOrUpdate.init(id);
+      });
     },
     // 删除
     deleteHandle(id) {
-      var ids = id ? [id] : this.dataListSelections.map(item => {
-        return item.auctionId
-      })
-      this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
+      var ids = id
+        ? [id]
+        : this.dataListSelections.map((item) => {
+            return item.auctionId;
+          });
+      this.$confirm(
+        `确定对[id=${ids.join(",")}]进行[${id ? "删除" : "批量删除"}]操作?`,
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      ).then(() => {
         this.$http({
-          url: this.$http.adornUrl('/product/auction/delete'),
-          method: 'post',
-          data: this.$http.adornData(ids, false)
+          url: this.$http.adornUrl("/product/auction/delete"),
+          method: "post",
+          data: this.$http.adornData(ids, false),
         }).then(({ data }) => {
           if (data && data.code === 0) {
             this.$message({
-              message: '操作成功',
-              type: 'success',
+              message: "操作成功",
+              type: "success",
               duration: 1500,
               onClose: () => {
-                this.getDataList()
-              }
-            })
+                this.getDataList();
+              },
+            });
           } else {
-            this.$message.error(data.msg)
+            this.$message.error(data.msg);
           }
-        })
-      })
-    }
-  }
-}
+        });
+      });
+    },
+  },
+};
 </script>
