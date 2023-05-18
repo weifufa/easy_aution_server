b9fd48ce80235a0414d14653ccad3a9f2bac0f7d
韦ff <1461531243@qq.com>
2023-05-18 21:24:43 +0800

    重新打包

--- static/config/index.js
+++ static/config/index.js
@@ -5,8 +5,8 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  // window.SITE_CONFIG['baseUrl'] = 'http://localhost:99/api';
-  window.SITE_CONFIG['baseUrl'] = 'http://www.weifufa.cn:99/api';
+window.SITE_CONFIG['baseUrl'] = 'http://localhost:99/api';
+  
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './'; // 域名
   window.SITE_CONFIG['version'] = '';   // 版本号(年月日时分)
